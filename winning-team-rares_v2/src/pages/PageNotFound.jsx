9bc1d253e6063a7204970c51d7264e09c247d591
Eugen M. <geanyzz91@gmail.com>
2024-04-07 08:18:09 +0300

    deleted old Rares' push

--- winning-team-rares_v2/src/pages/PageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./PageNotFound.module.css";
-
-export default function PageNotFound() {
-  return (
-    <div className={styles.notFound}>
-      <h1>404 - Page not found</h1>
-    </div>
-  );
-}
